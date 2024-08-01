67e9bee10f67db9bb16f37523d26497969c4a942
zoyron <sgrarora3@gmail.com>
2024-08-01 23:19:08 +0530

    completed the flag shader

--- shaders/src/shaders/test/fragment.glsl
+++ shaders/src/shaders/test/fragment.glsl
@@ -3,9 +3,11 @@ precision mediump float;
 uniform sampler2D uTexture;
 
 varying vec2 vUv;
+varying float vElevation;
 // varying float vRandom;
 
 void main() {
   vec4 textureColor = texture2D(uTexture, vUv);
+  textureColor.rgb *= vElevation*2.0 + 0.5;
   gl_FragColor = textureColor;
 }
--- shaders/src/shaders/test/vertex.glsl
+++ shaders/src/shaders/test/vertex.glsl
@@ -10,15 +10,18 @@ attribute vec2 uv;
 
 varying float vRandom;
 varying vec2 vUv;
+varying float vElevation;
 
 void main(){
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
-  modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
+  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1; 
+  modelPosition.z += elevation;
   // modelPosition.z += aRandom * 0.1;
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
   gl_Position = projectedPosition;
   vRandom = aRandom;
   vUv = uv;
+  vElevation = elevation;
 }
