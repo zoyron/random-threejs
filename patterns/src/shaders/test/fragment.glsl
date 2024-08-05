03ac71a81299afba859410f81bd3d8a1f4f22751
zoyron <sgrarora3@gmail.com>
2024-08-05 18:48:56 +0530

    10 patterns done

--- patterns/src/shaders/test/fragment.glsl
+++ patterns/src/shaders/test/fragment.glsl
@@ -1,4 +1,23 @@
+varying vec2 vUv;
+
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    // float strength = vUv.y;
+
+    // pattern 6
+    // float strength = vUv.y * 10.0;
+
+    // pattern 7
+    // float strength = mod((vUv.y * 10.0), 1.0);
+
+    // pattern 8
+    // float strength = mod((vUv.y * 10.0), 1.0);
+    // strength = step(0.5, strength);
+
+    // pattern 9
+    float strength = mod((vUv.y * 10.0), 1.0);
+    strength = step(0.8, strength);
+
+
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
