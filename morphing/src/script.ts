ef056cb9322a9ce9af91b6f77ac464cd546961a5
zoyron <sgrarora3@gmail.com>
2024-12-30 14:53:17 +0530

    loaded the models

--- morphing/src/script.ts
+++ morphing/src/script.ts
@@ -49,10 +49,12 @@ window.addEventListener("resize", () => {
   sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
   // Materials
-  particles.material.uniforms.uResolution.value.set(
-    sizes.width * sizes.pixelRatio,
-    sizes.height * sizes.pixelRatio
-  );
+  if (particles) {
+    particles.material.uniforms.uResolution.value.set(
+      sizes.width * sizes.pixelRatio,
+      sizes.height * sizes.pixelRatio
+    );
+  }
 
   // Update camera
   camera.aspect = sizes.width / sizes.height;
@@ -96,38 +98,62 @@ gui.addColor(debugObject, "clearColor").onChange(() => {
 });
 renderer.setClearColor(debugObject.clearColor);
 
-/**
- * Particles
- * instead of creating just a particles variable, we created it an object
- * since, lil-gui only acts on objects, hence this is done so that we can use lil-gui to morph it into other things
- */
-const particles: any = {};
+// Load Models
+let particles: any = null;
+gltfLoader.load("./models.glb", (gltf) => {
+  /**
+   * Particles
+   * instead of creating just a particles variable, we created it an object
+   * since, lil-gui only acts on objects, hence this is done so that we can use lil-gui to morph it into other things
+   */
+  particles = {};
 
-// Geometry
-particles.geometry = new THREE.SphereGeometry(3);
+  /**
+   * we want to extract the geometries from the objects in the models
+   * more specifically, we want the "position" attribute of the models
+   */
+  // Positions
+  // the purpose of the map function is to return something new from the array, to exract something from each child element in the array
+  gltf.scene.children.map((child) => {
+    console.log(child);
+  });
 
-/**
- * The idea
- */
+  // Geometry
+  particles.geometry = new THREE.SphereGeometry(3);
+  particles.geometry.setIndex(null);
 
-// Material
-particles.material = new THREE.ShaderMaterial({
-  vertexShader: particlesVertexShader,
-  fragmentShader: particlesFragmentShader,
-  uniforms: {
-    uSize: new THREE.Uniform(0.4),
-    uResolution: new THREE.Uniform(
-      new THREE.Vector2(
-        sizes.width * sizes.pixelRatio,
-        sizes.height * sizes.pixelRatio
-      )
-    ),
-  },
+  /**
+   * The idea is that we are going to send 2 sets of positions to vertex shader.
+   *
+   * Currently, the sphere we have is just one set of positions.
+   * Sphere particles will be named "position" because vertex shader expects one argument to be named "position", so why not just give the starting figure that variable name. This would be marked as the initial shape.
+   *
+   * And the shape we are targetting to morph into would be named as "aTargetPosition" while sending the position attributes of the new shape to the vertex shaders.
+   *
+   * And to morph between the two shapes, we will be using a uniform named "uProgress" that will range from 0 to 1, and will be used to mix between the two shapes
+   */
+
+  // Material
+  particles.material = new THREE.ShaderMaterial({
+    vertexShader: particlesVertexShader,
+    fragmentShader: particlesFragmentShader,
+    uniforms: {
+      uSize: new THREE.Uniform(0.4),
+      uResolution: new THREE.Uniform(
+        new THREE.Vector2(
+          sizes.width * sizes.pixelRatio,
+          sizes.height * sizes.pixelRatio
+        )
+      ),
+    },
+    blending: THREE.AdditiveBlending, // it draws the fragments on top of the previous fragments making the result brighter. The colors combine to create a glowing luminous appearance
+    depthWrite: false,
+  });
+
+  // Points
+  particles.points = new THREE.Points(particles.geometry, particles.material);
+  scene.add(particles.points);
 });
-
-// Points
-particles.points = new THREE.Points(particles.geometry, particles.material);
-scene.add(particles.points);
 
 /**
  * Animate
--- morphing/src/shaders/fragment.glsl
+++ morphing/src/shaders/fragment.glsl
@@ -1,6 +1,17 @@
 void main()
 {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    vec2 uv = gl_PointCoord;
+
+    // distance from center for each pixel
+    float distanceToCenter = length(uv - 0.5);
+
+    // the formula used here is "smallNumber / distanceToCenter - doubleOfSmallNumber"
+    float alpha = 0.05/distanceToCenter - 0.1;
+
+    // setting the color of fragments based on their distance from center
+    // the darker the color, the nearer to the center, black means right at the center
+    gl_FragColor = vec4(vec3(1.0), alpha);
+
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
