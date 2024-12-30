dfd8bb049b200b53f108d329d3c80c2a24b4c819
zoyron <sgrarora3@gmail.com>
2024-12-30 11:20:27 +0530

    started the morphing project

---
+++ morphing/vite.config.ts
@@ -0,0 +1,24 @@
+import restart from 'vite-plugin-restart'
+import glsl from 'vite-plugin-glsl'
+
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+    server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
+    plugins:
+    [
+        restart({ restart: [ '../static/**', ] }), // Restart server on static file change
+        glsl() // Handle shader files
+    ]
+}
