decb84a918dd3ac7754f452e03009d1b04e98679
zoyron <sgrarora3@gmail.com>
2025-02-25 15:15:14 +0530

    add 2 noises, one for getting back as well

--- morphing/src/shaders/particles/fragment.glsl
+++ morphing/src/shaders/particles/fragment.glsl
@@ -1,9 +1,12 @@
+varying vec3 vColor;
+
 void main()
 {
     vec2 uv = gl_PointCoord;
     float distanceToCenter = length(uv - 0.5);
     float alpha = 0.05/distanceToCenter - 0.1;
     // gl_FragColor = vec4(1.0,1.0,1.0, alpha);
+    // gl_FragColor = vec4(vColor, alpha);
     gl_FragColor = vec4(1.0, uv, alpha);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
--- morphing/src/shaders/particles/vertex.glsl
+++ morphing/src/shaders/particles/vertex.glsl
@@ -1,12 +1,28 @@
 uniform vec2 uResolution;
 uniform float uSize;
 uniform float uProgress;
+
 attribute vec3 aPositionTarget;
+
+varying vec3 vColor;
+
+#include ../includes/simplexNoise3d.glsl
 
 void main()
 {
     // Mixed position
-    float progress = uProgress;
+    float noiseOrigin = simplexNoise3d(position);
+    float noiseTarget = simplexNoise3d(aPositionTarget);
+    float noise = mix(noiseOrigin, noiseTarget, uProgress);
+
+    // The simplex noise ranges from -1 to 1, we need to map it to a range of 0 to 1, hence this step
+    noise = smoothstep(-1.0, 1.0, noise);
+
+    float duration = 0.4;
+    float delay = (1.0 - duration) * noise;
+    float end = delay + duration;
+    float progress = smoothstep(delay, end, uProgress);
+
     vec3 mixedPosition = mix(position, aPositionTarget, progress);
 
     // Final position
@@ -18,4 +34,7 @@ void main()
     // Point size
     gl_PointSize = uSize * uResolution.y;
     gl_PointSize *= (1.0 / - viewPosition.z);
+
+    // update varyings
+    vColor = vec3(noise);
 }
