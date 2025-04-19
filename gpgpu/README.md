dd0dd842eca999c033b2114efffe617d0bc168bf
zoyron <sgrarora3@gmail.com>
2025-04-19 13:14:00 +0530

    update

--- gpgpu/README.md
+++ gpgpu/README.md
@@ -1,63 +1,16 @@
-# Basic Three.js Template
+# Three.js Journey
 
-## Now supports TypeScript as well
+## Setup
+Download [Node.js](https://nodejs.org/en/download/).
+Run this followed commands:
 
-A streamlined boilerplate for Three.js projects, designed to get you up and running quickly.
-
-![Boilerplate demo](./static/boilerplate.gif)
-
-## 🚀 Quick Start
-
-Follow these steps to set up your project using this boilerplate:
-
-```bash
-# Clone the repository and cd into the project
-git clone https://github.com/zoyron/threejs-boilerplate.git your-project-name
-cd your-project-name
-
-# Remove git history and start fresh with your own Git history
-rm -rf .git
-git init
-
-# Install dependencies
+``` bash
+# Install dependencies (only the first time)
 npm install
 
-# Run the development server
+# Run the local server at localhost:8080
 npm run dev
+
+# Build for production in the dist/ directory
+npm run build
 ```
-
-Your project is now running at http://localhost:5173 (or another port if 5173 is occupied).
-
-## Threejs Project Structure
-
-### Base Setup(includes the following):
-
-- Canvas
-- Scene
-- Sizes(the object)
-
-### The Camera, groups(optional) and lights(optional):
-
-- Perspective Camera(most usualy and popular choice)
-- Lights(optional, with ambient light being the most popular choice)
-
-### Creating an object and adding it to scene
-
-- Creating geometry or points-geometry; could be inbuilt, custom, random or using shaders
-- Creating material or points-material; could be inbuilt, custom, random or using shaders
-- Creating mesh or points; could be inbuilt, custom, random or using shaders
-
-> above three are the points where we mostly use shaders
-
-- Adding the created mesh or points to the scene or group
-
-### Renderer and Resizing
-
-- Adding a scene renderer
-- Adding a window resizing event listener, and updating the render inside it
-
-### Controls and Animate function
-
-- Adding orbit controls and damping them if required
-- adding the animate() function, and giving necessary logic and functionality to it
-- calling the animate() function
---
+++ gpgpu/src/script.js
@@ -0,0 +1,150 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+import GUI from "lil-gui";
+import particlesVertexShader from "./shaders/particles/vertex.glsl";
+import particlesFragmentShader from "./shaders/particles/fragment.glsl";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI({ width: 340 });
+const debugObject = {};
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Loaders
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/draco/");
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+  // Materials
+  particles.material.uniforms.uResolution.value.set(
+    sizes.width * sizes.pixelRatio,
+    sizes.height * sizes.pixelRatio
+  );
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(sizes.pixelRatio);
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(4.5, 4, 11);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(sizes.pixelRatio);
+
+debugObject.clearColor = "#29191f";
+renderer.setClearColor(debugObject.clearColor);
+
+/**
+ * Particles
+ */
+const particles = {};
+
+// Geometry
+particles.geometry = new THREE.SphereGeometry(1.5);
+
+// Material
+particles.material = new THREE.ShaderMaterial({
+  vertexShader: particlesVertexShader,
+  fragmentShader: particlesFragmentShader,
+  uniforms: {
+    uSize: new THREE.Uniform(0.1),
+    uResolution: new THREE.Uniform(
+      new THREE.Vector2(
+        sizes.width * sizes.pixelRatio,
+        sizes.height * sizes.pixelRatio
+      )
+    ),
+  },
+});
+
+// Points
+particles.points = new THREE.Points(particles.geometry, particles.material);
+scene.add(particles.points);
+
+/**
+ * Tweaks
+ */
+gui.addColor(debugObject, "clearColor").onChange(() => {
+  renderer.setClearColor(debugObject.clearColor);
+});
+gui
+  .add(particles.material.uniforms.uSize, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uSize");
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+
+  // Update controls
+  controls.update();
+
+  // Render normal scene
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
